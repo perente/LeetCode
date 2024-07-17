783101422467a088c795896deeaa25f89d74923a
rampia perente <157977666+perente@users.noreply.github.com>
2024-07-17 23:22:47 +0300

    Time: 0 ms (100.00%) | Memory: 8.5 MB (27.72%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x < 0 || x != 0 && x % 10 == 0) 
+        {
+            return false;
+        }
+        
+        int l_r = x;
+        int r_l = 0;
+        while(x > 0)
+        {
+            int digit = x % 10;
+
+            if (r_l > (numeric_limits<int>::max() - digit) / 10)
+            return false;
+
+            r_l = digit + r_l*10;
+            x /= 10; 
+        }
+
+        if(r_l == l_r)
+        return true;
+        else 
+        return false;
+    }
+};
