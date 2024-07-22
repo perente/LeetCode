f94779874c51f28d60491733ee41289071da40ee
rampia perente <157977666+perente@users.noreply.github.com>
2024-07-22 21:26:50 +0300

    Time: 79 ms (67.61%) | Memory: 62.4 MB (10.39%) - LeetSync

---
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int left = 0;
+        int right = arr.size() - 1;
+
+        while(left < right)
+        {
+            int middle = left + (right - left)/2;
+            if(arr[middle] < arr[middle + 1])
+            left = middle + 1;
+            else
+            right = middle;
+        }
+        return left;
+    }
+};
