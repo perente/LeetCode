ac9093c5150a9111d61167e817d583bcc24bfc3d
rampia perente <157977666+perente@users.noreply.github.com>
2024-07-22 17:01:29 +0300

    Time: 0 ms (100.00%) | Memory: 8.1 MB (13.13%) - LeetSync

---
+++ 69-sqrtx/sqrtx.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x == 0)
+        return 0;
+
+        int left = 1;
+        int right = x;
+        int sqrtx;
+
+        while(left <= right)
+        {
+            int middle = left + (right - left)/2;
+            if(middle <= x/middle)
+            {
+                sqrtx = middle;
+                left = middle + 1;
+            } else
+            right = middle - 1;
+        }
+        return sqrtx;
+    }
+};
