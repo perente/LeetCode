a95aaa0e0026504fecd5cc3a65e1155594b8a3a0
rampia perente <157977666+perente@users.noreply.github.com>
2024-07-29 18:22:44 +0300

    Time: 180 ms (38.44%) | Memory: 126.1 MB (23.68%) - LeetSync

---
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        stack<int> paldom;
+        ListNode* current = head;
+
+        while(current != NULL)
+        {
+            paldom.push(current->val);
+            current = current->next;
+        }
+
+        current = head;
+        while(current != NULL)
+        {
+            if(current->val != paldom.top())
+            return false;
+
+            paldom.pop();
+            current = current->next;
+        }
+        return true;
+    }
+};
