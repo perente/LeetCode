f57851be6e3c995067d4d1b8daba7b3f39230124
rampia perente <157977666+perente@users.noreply.github.com>
2024-08-01 17:59:07 +0300

    Time: 3 ms (37.67%) | Memory: 11.4 MB (8.84%) - LeetSync

---
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -0,0 +1,29 @@
+class Solution {
+//Rampia Perente
+
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        vector<int> final(m + n);
+
+        int p1 = 0;
+        int p2 = 0;
+        int pf = 0;
+
+        while(p1 < m && p2 < n)
+        {
+            if(nums1[p1] < nums2[p2])
+            final[pf++] = nums1[p1++];
+            else
+            final[pf++] = nums2[p2++];
+        }
+
+        while(p1 < m)
+        final[pf++] = nums1[p1++];
+
+        while(p2 < n)
+        final[pf++] = nums2[p2++];
+
+        for(int i = 0; i < m + n; i++)
+        nums1[i] = final[i];
+    }
+};
