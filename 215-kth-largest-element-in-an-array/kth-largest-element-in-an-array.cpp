05d01d6ac8b8edaf13bc1e2246af0455a10987d6
rampia perente <157977666+perente@users.noreply.github.com>
2024-08-18 02:16:09 +0300

    Time: 92 ms (62.83%) | Memory: 61.5 MB (43.54%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -2,37 +2,11 @@
 
 class Solution {
 public:
-    int partition(vector<int>& nums, int left, int right) {
-        int pivot = nums[right];
-        int i = left;
-        for (int j = left; j < right; j++) 
-        {
-            if (nums[j] > pivot)
-            { 
-                swap(nums[i], nums[j]);
-                i++;
-            }
-        }
-        swap(nums[i], nums[right]);
-        return i;
-    }
-
-    int quickSelect(vector<int>& nums, int left, int right, int k) {
-        if(left == right) 
-        return nums[left];
-
-        int pivot = partition(nums, left, right);
-
-        if(pivot == k) 
-        return nums[pivot];
-        else if (pivot < k) 
-        return quickSelect(nums, pivot + 1, right, k);
-        else 
-        return quickSelect(nums, left, pivot - 1, k);
-    }
-
     int findKthLargest(vector<int>& nums, int k) {
-        int n = nums.size();
-        return quickSelect(nums, 0, n - 1, k - 1);
+        priority_queue<int> max_heap(nums.begin(), nums.end());
+        for(int i = 1; i < k; i++)
+        max_heap.pop();
+
+        return max_heap.top();
     }
 };
