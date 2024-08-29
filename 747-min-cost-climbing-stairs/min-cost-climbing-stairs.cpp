3372508b7dfad4b2541dd0d239249fdfa649d829
rampia perente <157977666+perente@users.noreply.github.com>
2024-08-29 20:28:59 +0300

    Time: 0 ms (100.00%) | Memory: 16.8 MB (23.41%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
@@ -9,7 +9,7 @@ public:
         min_cost[0] = cost[0];
         min_cost[1] = cost[1];
 
-        for(int i = 2; i < x; i++)
+        for(int i = 2; i <= x - 1; i++)
         min_cost[i] = min(min_cost[i - 1] + cost[i], min_cost[i - 2] + cost[i]);
 
         return min(min_cost[x - 1], min_cost[x - 2]);
