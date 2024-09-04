d776df3811983b151109e146619f0754dfe9c0ee
rampia perente <157977666+perente@users.noreply.github.com>
2024-09-04 10:16:51 +0300

    Time: 4 ms (21.90%) | Memory: 8.3 MB (40.77%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if (n == 0)
+        return 1;
+
+        if(n < 0) 
+        {
+            x = 1 / x;
+            
+            if(n == INT_MIN)
+            {
+                n = INT_MAX;
+                x *= x;
+            }
+            else 
+            n = -n;
+        }
+
+        double result = 1;
+
+        while(n > 0)
+        {
+            if(n % 2 == 1)
+            result *= x;
+
+            x *= x;
+            n /= 2;
+        }
+
+        return result;
+    }
+};
